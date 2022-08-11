bdcd6555f8f9ede8a66dc36bb80e6315a8ce30f7
andersgom <asgomez95@gmail.com>
2022-08-11 21:27:54 +0100

    app done, .gitignore added

--- app.py
+++ app.py
@@ -1,60 +1,65 @@
-import streamlit as st #first thing we do is import the streamlit library which we need to create the app
-import pandas as pd #import pandas if you are going to use excel or csv tables
+import streamlit as st
+import pandas as pd
 from final_script import *
 import streamlit.components.v1 as components
 
-#Firstly let us run our streamlit app so we see our changes happening live.
 
-headers = st.container() #create a variable for your main header
+headers = st.container()
 model_training = st.container() 
 
-with headers: #with is for us to get inside this section
-    st.title('Macarenameter') #st.title allow us to create the main title 
-    st.text("""Check if your song is a potential summer hit in Spain.""") #st.text allow us to write text
+with headers:
+    st.title('Macarenameter',)
+    st.markdown("""#### Check if your song qualifies as a potential summer hit in Spain 🏖️""", unsafe_allow_html=False)
+    st.markdown("""We analized the summer hits in Spain of the last 30 years, so you can know if your song would be a potential summer hit in the confort fo your home.
+     Chek the full analysis in our [GitHub Repo](https://github.com/andersgom/project5-week6).""", unsafe_allow_html=False)
 
 with model_training:
-    st.header('')
-    sel_col, disp_col = st.columns(2) #choose how many columns we want to work with
-    #now we are creating a slider for user to choose what value he wants
-    input_feature = sel_col.text_input('Write an opinion?',' ')
-    st.write('You wrote: ', input_feature)
+    sel_col, disp_col = st.columns(2)
+    input_feature = sel_col.text_input('Insert your song url','')
 
     test = ''
     if len(input_feature) > 50:
+        st.markdown("""### Single song results:""", unsafe_allow_html=False)
         test = frame_maker(input_feature)
 
         if test['Hit'].values == 1:
-            st.write("Your song is a potential summer hit, amigo.")
+            st.write("Your song qualifies for a potential summer hit, amigo.")
         else:
             st.write("Good luck next time.")
 
 
         minipvar = input_feature[:25]+'embed/'+input_feature[25:]
-        st.write(minipvar[:-20])
         miniplayer = f"""
         <iframe style="border-radius:12px" src="{minipvar[:-20]}?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
         """
 
-        components.html(miniplayer, height=600)
+        components.html(miniplayer, height=400)
 
 
     else:
-        st.write("testtttttt")
+        st.write("")
 
-#testlink = "https://open.spotify.com/track/7JIjUx3GsL0upxmNJacmtz?si=d4660d663dcf4150"
+    input_feature = sel_col.text_input('Insert your playlist url','')
 
-#tl2 = testlink[:-20]
+    test = ''
+    if len(input_feature) > 50:
+        st.markdown("""### Playlist results:""", unsafe_allow_html=False)
+        test = playlist_dataframe(input_feature)
 
-#tl2[:25]+'embed/'+tl2[25:]
+        if len(test[test['Hit']==1][['artist_name', 'track_name', 'release_date']]) > 0:
+            st.write("From your playlist, these tracks qualifiy as potential hits.")
+            st.write(test[test['Hit']==1][['artist_name', 'track_name', 'release_date']])
+        else:
+            st.write("None of your tracks qualify.")
+        
 
-    #minipvar = 
 
+        minipvar = input_feature[:25]+'embed/'+input_feature[25:]
+        miniplayer = f"""
+        <iframe style="border-radius:12px" src="{minipvar[:-20]}?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        """
 
-#testlink = "https://open.spotify.com/embed/track/5ildQOEKmJuWGl2vRkFdYc"
+        components.html(miniplayer, height=400)
 
-
-#https://open.spotify.com/track/7JIjUx3GsL0upxmNJacmtz?si=d4660d663dcf4150
-
-#https://open.spotify.com/embed/track/5ildQOEKmJuWGl2vRkFdYc
-#https://open.spotify.com/embed/track/7MJQ9Nfxzh8LPZ9e9u68Fq
-    #<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7JIjUx3GsL0upxmNJacmtz?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+    else:
+        st.write(" ")
---
+++ keys.py
@@ -0,0 +1,2 @@
+client_id = '94c7d23dac2a4225a3ecb57cce99966c'
+client_secret = "5a0d909aa4f74df9b98153f5a6cd4a60"
