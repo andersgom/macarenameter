4070437bbe04345c2f094c6e84358dc0f7c8cfb7
Rui Ferreira <ruiferreira1992@gmail.com>
2022-08-17 17:53:22 +0100

    no keys

--- final_script.py
+++ final_script.py
@@ -6,8 +6,8 @@ import numpy as np
 
 
 
-client_id = '94c7d23dac2a4225a3ecb57cce99966c' #getpass.getpass()
-client_secret = "5a0d909aa4f74df9b98153f5a6cd4a60" # getpass.getpass()
+client_id = '' #getpass.getpass()
+client_secret = "" # getpass.getpass()
 
 
 
